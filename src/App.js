1efb5b25c89148006d36a9250698f402d1e865fa
mrsebastino <mrsebastino@gmail.com>
2021-03-25 14:25:15 +0000

    add an array for useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTask] = useState([
+    {
+        id: 1,
+        text: "Doctor appointment",
+        day: "Feb 5th at 14.30",
+        reminder: true,
+
+    },
+    {
+        id: 2,
+        text: "Concert at o2",
+        day: "Feb 6th at 20.30",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Food shopping",
+        day: "Feb 5th at 14.30",
+        reminder: false,
+    },
+])
   return (
     <div className="container">
      <Header />
-     
+     <Tasks tasks={tasks} />
     </div>
   );
 }
