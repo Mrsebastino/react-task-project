5fb77894b256db5b5f66a4775b6e6c941185560b
mrsebastino <mrsebastino@gmail.com>
2021-03-25 15:00:47 +0000

    add delete task functionality

--- src/App.js
+++ src/App.js
@@ -24,10 +24,22 @@ const App = () => {
         reminder: false,
     },
 ])
+
+// Delete Task
+
+const deleteTask = (id)=>{
+  setTask(tasks.filter((task) => task.id !== id))
+}
+
+
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks} />
+     {tasks.length > 0 ? ( 
+     <Tasks tasks={tasks} onDelete={deleteTask} />
+     ) : (
+       "No task to show"
+     )}
     </div>
   );
 }
